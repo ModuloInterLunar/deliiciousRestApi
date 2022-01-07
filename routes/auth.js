904256781be16cb9b5b32a56a957fb759cfb5a60
Djessy Czaplicki <djessyczaplicki@gmail.com>
2022-01-07 14:34:24 +0100

    - Added authentication using employees , password and tokens - Moved logPetition to services, and made it async - Employee creation and authentication (testing)

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const router = express.Router();
-const bcrypt = require('bcrypt');
+const services = require('../services');
 
-router.post('/register', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = {
-            _id: req.body._id,
-            type: req.body.type,
-        }
-    } catch (err) {
-        
+function isAuth(req, res, next) {
+    if (!req.headers.authorization){ 
+        console.log(`Authentication failed from: '${req.ip}'`);
+        return res.status(401).send({ message: "You do not have authorization!"});
     }
-});
 
-module.exports = router;
+    const token = req.headers.authorization.split(' ')[1];
+
+    services.decodeToken(token)
+        .then( response => {
+            req.user = response;
+            next();
+        })
+        .catch( response => {
+            res.status(response.status).send(response.message);
+        });
+}
+
+module.exports = isAuth;
