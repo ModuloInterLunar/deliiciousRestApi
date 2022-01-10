64ae7919ae53a460ca9913e1b3132b8b7e9e46c1
Djessy Czaplicki <djessyczaplicki@gmail.com>
2022-01-10 01:35:26 +0100

    Uso de _id Fixes #8

---
+++ models/employee.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+    _id: { type: String, required: true },
+    username: { type: String, required: true},
+    dni: {type: String, required: true},
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true }
+});
+
+employeeSchema.virtual('id').get(() => this._id);
+
+module.exports = mongoose.model('Employee', employeeSchema);
