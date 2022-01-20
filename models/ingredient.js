7902e54bb1a1d34acd4547b4eb6579bbac94c9f5
Alvaro Martinez <alvaromagu2002@gmail.com>
2022-01-20 15:40:14 +0100

    Change tables and ingredients variables to Number

--- models/ingredient.js
+++ models/ingredient.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const ingredientSchema = new mongoose.Schema({
     _id: { type: String, required: true },
     name: { type: String, required: true, unique: true},
-    price: { type: "Decimal128", required: true },
-    quantity: { type: "Decimal128", required: true }
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true }
 });
 
 module.exports = mongoose.model('Ingredients', ingredientSchema);
