e439b086dc4636fb847b269bde275e1d30455a3a
Djessy Czaplicki <djessyczaplicki@gmail.com>
2022-01-23 19:16:38 +0100

    - Changed token expiration - Set a variable for the token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,8 +2,8 @@ const services = require('../services');
 
 function isAuth(req, res, next) {
     if (!req.headers.authorization){ 
-        console.log(`Authentication failed from: '${req.ip}'`);
-        return res.status(401).send({ message: "You do not have authorization!"});
+        console.log(`Authentication failed; 'Token is missing' from: '${req.ip}'`);
+        return res.status(401).send({ message: "Token is missing"});
     }
 
     const token = req.headers.authorization.split(' ')[1];
