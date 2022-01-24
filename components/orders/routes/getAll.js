085e9f4fb7e7e98c5f729f8b15a8ce80f5996eb5
Djessy Czaplicki <djessyczaplicki@gmail.com>
2022-01-24 22:16:43 +0100

    - removed password from the employee sent with the orders

--- components/orders/routes/getAll.js
+++ components/orders/routes/getAll.js
@@ -2,7 +2,7 @@ const Order = require('../../../models/order');
 
 const getAll = async (req, res) => {
     try {
-        const orders = await Order.find().populate('employee').populate('ticket');
+        const orders = await Order.find().populate('employee', "-password").populate('ticket');
         res.json(orders);
     } catch (err) {
         res.status(500).json({ message: err.message});
