962ee16510a4624ce56f811a19d6a2d2f6fb65f2
Djessy Czaplicki <djessyczaplicki@gmail.com>
2022-01-30 13:55:14 +0100

    - Fixed issue in updateOne dish

--- components/dishes/routes/updateOne.js
+++ components/dishes/routes/updateOne.js
@@ -5,7 +5,7 @@ const updateOne = async (req, res) => {
     if (req.body.name) res.dish.name = req.body.name;
     if (req.body.type) res.dish.type = req.body.type;
     if (req.body.price) res.dish.price = req.body.price;
-    if (res.dish.ingredientQties) res.dish.ingredientQties = req.body.ingredientQties;
+    if (req.body.ingredientQties) res.dish.ingredientQties = req.body.ingredientQties;
     if (req.body.description) res.dish.description = req.body.description;
     if (req.body.image) res.dish.image = req.body.image;
     try {
